5098aa37b878c6112523f22d980d25076da7b6d2
Kuzmenko Serhii <110684653+fate-it@users.noreply.github.com>
2026-03-12 12:47:50 +0200

    Refactor guessing game into a function

--- HW10/main.py
+++ HW10/main.py
@@ -1,23 +1,25 @@
 import random
 
 
-number = random.randint(1, 100)  
-attempts = 5
-while(attempts !=0 ):
-    try:
-        user_input = int(input("Вгадайте число від 1 до 100: "))
-    except ValueError:
-        print("Ви ввели не число! Спробуйте ще раз.")
-        continue
+def game():
+    number = random.randint(1, 100)  
+    attempts = 5
+    while(attempts !=0 ):
+        try:
+            user_input = int(input("Вгадайте число від 1 до 100: "))
+        except ValueError:
+            print("Ви ввели не число! Спробуйте ще раз.")
+            continue
+    
+        if user_input == number:
+            print("Вітаємо! Ви вгадали правильне число")
+            exit()
+        elif user_input > number:
+            print("Занадто високо")
+        else:
+            print("Занадто низько")
+        attempts -= 1
+        print(f"Залишилось спроб: {attempts}")
+    print(f"Вибачте, у вас закінчилися спроби. Правильний номер був {number}")
 
-    if user_input == number:
-        print("Вітаємо! Ви вгадали правильне число")
-        exit()
-    elif user_input > number:
-        print("Занадто високо")
-    else:
-        print("Занадто низько")
-    attempts -= 1
-    print(f"Залишилось спроб: {attempts}")
-print(f"Вибачте, у вас закінчилися спроби. Правильний номер був {number}")
-
+game()
